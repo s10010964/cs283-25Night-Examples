0977bbbcfc952b365001af78d504f71a4997ba75
s10010964 <s10010964@gmail.com>
2017-07-16 21:42:35 +0800

    新增了學生管理套件與使用基本範例

--- Lecture01-Examples/Example01/Program.cs
+++ Lecture01-Examples/Example01/Program.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Example01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
+            Console.Write("請輸入帳號");
+            Console.WriteLine("我今天心情很{0}{1}", "開", "心");
         }
     }
 }
